d6a2b7cd2e86a1393d02e2cec3bc3a200897486e
Manjunath-Singh-V <manjunathsinghv5@gmail.com>
2021-08-23 17:28:59 +0530

    commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer style="background-color: powderblue;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12" style="color: black;">
+                    &copy; 2020 Online Course Registration | By : <a href="#" target="_blank" style="color: black;">Manjunath Singh V and Manjunath R</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ admin/includes/header.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero" style="background-color: powderblue;">
+        <div class="container">
+            <div class="navbar-header" style="margin-top: 10px;">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                 <a class="navbar-brand" href="#" style="color: black; font-size:24px;4px; line-height:24px; ">
+
+                   Online Course Registration
+                </a>
+            </div>
+
+            <div class="left-div">
+                <i class="fa fa-user-plus login-icon" style="background-color: blue;"></i>
+        </div>
+            </div>
+        </div>
